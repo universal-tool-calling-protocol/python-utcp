8e4e12661cc832b0213cc1b7f7f8788e856b57d8
Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>
2025-07-17 17:43:25 +0200

    Update version

--- src/utcp/version.py
+++ src/utcp/version.py
@@ -2,7 +2,7 @@ from importlib.metadata import version, PackageNotFoundError
 import tomli
 from pathlib import Path
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 try:
     __version__ = version("utcp")
 except PackageNotFoundError:
