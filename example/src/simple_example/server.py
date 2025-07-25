1c818c86f2694bc705761d0bab96384cbc3d909d
Andrei Ghiurtu <andreibv96@yahoo.com>
2025-07-25 11:24:25 +0300

    Parse jsonschema of tools by hand
    * chore: add gql to dependencies list, fix license, pyproject.toml (#11)
    
    * v0.1.4
    
    * fix in memory repository variables (#15)
    
    * chore: add gql to dependencies list, fix license, pyproject.toml
    
    * fix: make tools an instanse variables
    
    * v0.1.5
    
    * v0.1.6
    
    * v0.1.7
    
    Fix issue of shared memory of UTCPClients
    
    * Update version
    
    * parse by hand
    
    ---------
    
    Co-authored-by: Ulugbek <46451363+ulughbeck@users.noreply.github.com>
    Co-authored-by: Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>

--- example/src/simple_example/server.py
+++ example/src/simple_example/server.py
@@ -1,12 +1,19 @@
+from typing import List, Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from utcp.shared.provider import HttpProvider
 from utcp.shared.tool import utcp_tool
 from utcp.shared.utcp_manual import UtcpManual
 
+class TestInput(BaseModel):
+    value: str
 
 class TestRequest(BaseModel):
     value: str
+    arr: List[TestInput]
+
+class TestResponse(BaseModel):
+    received: str
 
 __version__ = "1.0.0"
 BASE_PATH = "http://localhost:8080"
@@ -23,5 +30,12 @@ def get_utcp():
     http_method="POST"
 ))
 @app.post("/test")
-def test_endpoint(data: TestRequest):
-    return {"received": data.value}
+def test_endpoint(data: TestRequest) -> Optional[TestResponse]:
+    """Test endpoint to receive a string value.
+    
+    Args:
+        data (TestRequest): The input data containing a string value.
+    Returns:
+        TestResponse: A dictionary with the received value.
+    """
+    return TestResponse(received=data.value)
